d027fe67dd6473b322ce5fde58cb7c5e06abfc16
Igor Camilo <igor.rcamilo@gmail.com>
2017-11-27 12:22:49 -0200

    tvOS target added.

---
+++ Bit2Common-tvOS/Bit2Common_tvOS.h
@@ -0,0 +1,19 @@
+//
+//  Bit2Common_tvOS.h
+//  Bit2Common-tvOS
+//
+//  Created by Igor Camilo on 27/11/17.
+//  Copyright © 2017 Bit2 Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Bit2Common_tvOS.
+FOUNDATION_EXPORT double Bit2Common_tvOSVersionNumber;
+
+//! Project version string for Bit2Common_tvOS.
+FOUNDATION_EXPORT const unsigned char Bit2Common_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bit2Common_tvOS/PublicHeader.h>
+
+
