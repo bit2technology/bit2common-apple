c7e58851528ee89ffd475ac27abd829b9879ae62
Igor Camilo <igor.rcamilo@gmail.com>
2017-11-27 11:53:27 -0200

    watchOS target added.

---
+++ Bit2Common-watchOS/Bit2Common_watchOS.h
@@ -0,0 +1,19 @@
+//
+//  Bit2Common_watchOS.h
+//  Bit2Common-watchOS
+//
+//  Created by Igor Camilo on 27/11/17.
+//  Copyright © 2017 Bit2 Technology. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for Bit2Common_watchOS.
+FOUNDATION_EXPORT double Bit2Common_watchOSVersionNumber;
+
+//! Project version string for Bit2Common_watchOS.
+FOUNDATION_EXPORT const unsigned char Bit2Common_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bit2Common_watchOS/PublicHeader.h>
+
+
